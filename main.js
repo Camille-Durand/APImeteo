fab8806b37adddfdc45a1acaa42aed81121f051a
Camille-Durand <camillefanguin@gmail.com>
2023-11-22 13:41:57 +0100

    Test fetch() API meteo

---
+++ main.js
@@ -0,0 +1,24 @@
+let url = "https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=6f88826d767f323ad19cd282444924e9";
+const apiMeteo = fetch(url)
+                    .then(async response=> {
+                        console.log(response);
+                        const json = await response.json();
+                        console.log(json);
+                        console.log("Meteo:",json.weather[0].description);
+
+                        let meteo = json.weather[0].description;
+                        let temp = json.main.temp - 273.1;
+                        let humidity = json.main.humidity;
+                        let town = json.name;
+                        let lat = json.coord.lat;
+                        let long = json.coord.lon;
+
+                        let infoMeteo = document.querySelector('h1');
+                        let infos = document.querySelectorAll('h2');
+                        infoMeteo.textContent = meteo;
+                        infos[0].textContent = "Temperature: " + temp.toFixed(2) + "°c";
+                        infos[1].textContent = "Humidity: " + humidity + "%";
+                        infos[2].textContent = "Town: " + town;
+                        infos[3].textContent = "Latitude: " + lat;
+                        infos[4].textContent = "Longitude: " + long;
+                    });
